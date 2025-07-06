96b294b43cb4a36a0f51a00cc65c61c5fdcf2e5f
Ilia Proskurin <pilailiapro@gmail.com>
2025-07-06 08:19:15 +0300

    correct react hook form

--- registration/src/App.jsx
+++ registration/src/App.jsx
@@ -12,25 +12,29 @@ const fieldsSchema = yup.object().shape({
     .string()
     .matches(
       /^[\w_.@]*$/,
-      "Неверный email. Допустимые символы: буквы, цифры,@, и нижнее подчёркивание"
+      "Неверный email. Допустимые символы: буквы, цифры,@, и нижнее подчёркивание."
     ),
   password: yup
     .string()
     .matches(
       /^[\w_]*$/,
-      "Неверный пароль. Допустимые символы: буквы, цифры и нижнее подчёркивание"
+      "Неверный пароль. Допустимые символы: буквы, цифры и нижнее подчёркивание."
     )
-    .min(3, "Пароль меньше 3 символов"),
+    .min(3, "Пароль меньше 3 символов."),
   confirmPassword: yup
     .string()
-    .oneOf([yup.ref("password")], "Пароли не совпадают"),
+    .oneOf([yup.ref("password")], "Пароли не совпадают."),
 });
+
+const InputForm = ({ ...register }) => {
+  return <input name="email" type="text" {...register("email")} />;
+};
 
 export const App = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm({
     defaultValues: {
       email: "",
@@ -40,24 +44,31 @@ export const App = () => {
     resolver: yupResolver(fieldsSchema),
   });
 
-  const loginError = Object.keys(errors).reduce(
+  const Error = Object.keys(errors).reduce(
     (acc, key) => acc + errors[key]?.message,
     ""
   );
+  const inputsState = {
+    email: { name: "email", type: "text" },
+    password: { name: "password", type: "password" },
+    confirmPassword: { name: "confirmPassword", type: "password" },
+  };
 
   return (
     <div className={styles.app}>
       <form onSubmit={handleSubmit(sendFormData)}>
-        {loginError && <div className={styles.errorLabel}>{loginError}</div>}
-        <input name="email" type="text" {...register("email")} />
-        <input name="password" type="password" {...register("password")} />
-        <input
-          name="confirmPassword"
-          type="password"
-          {...register("confirmPassword")}
-        />
-
-        <button type="submit" disabled={!!loginError}>
+        {Error && <div className={styles.errorLabel}>{Error}</div>}
+        {Object.keys(inputsState).map((key, index) => {
+          return (
+            <input
+              key={index}
+              name={inputsState[key].name}
+              type={inputsState[key].type}
+              {...register(inputsState[key].name)}
+            />
+          );
+        })}
+        <button type="submit" disabled={!isValid}>
           Отправить
         </button>
       </form>
